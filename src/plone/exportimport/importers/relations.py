b438abf4d4a912a4c0fcadd620a6ce2769f440c4
Víctor Fernández de Alba <sneridagh@gmail.com>
2024-05-04 01:41:54 +0200

    Reindex members of relations in case that they contain preview_image_links (#13)
    * Reindex members of relations in case that they contain preview_image_links
    
    * Changelog

--- src/plone/exportimport/importers/relations.py
+++ src/plone/exportimport/importers/relations.py
@@ -1,4 +1,5 @@
 from .base import BaseImporter
+from plone import api
 from plone.exportimport import logger
 from plone.exportimport.utils import relations as utils
 
@@ -13,4 +14,10 @@ class RelationsImporter(BaseImporter):
             return f"{self.__class__.__name__}: No data to import"
         logger.debug(f"- Relations: Read {len(data)} from {self.filepath}")
         results = utils.set_relations(data)
+        for item in results:
+            # Reindex relations objects in case they contain
+            #  `preview_image_link` fields to update the image scales
+            api.content.get(UID=item["to_uuid"]).reindexObject()
+            api.content.get(UID=item["from_uuid"]).reindexObject()
+
         return f"{self.__class__.__name__}: Imported {len(results)} relations"
