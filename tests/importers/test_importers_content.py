e21687ea4eaf6d419b3b150bbbf654e289ae555e
Érico Andrei <ericof@gmail.com>
2024-06-10 16:02:14 -0300

    Export / Import local permissions for each content (Fix #15)

--- tests/importers/test_importers_content.py
+++ tests/importers/test_importers_content.py
@@ -28,3 +28,34 @@ class TestImporterContent:
         result = importer.import_data(base_path=empty_import_path)
         assert isinstance(result, str)
         assert result == "ContentImporter: No data to import"
+
+
+class TestImporterLocalPermissions:
+    @pytest.fixture(autouse=True)
+    def _init(self, portal, base_import_path, load_json):
+        self.portal = portal
+        importer = content.ContentImporter(portal)
+        importer.import_data(base_path=base_import_path)
+
+    @pytest.mark.parametrize(
+        "uid,permission_name,roles",
+        [
+            [
+                "35661c9bb5be42c68f665aa1ed291418",
+                "plone.app.contenttypes: Add Image",
+                ["Manager"],
+            ],
+            [
+                "e7359727ace64e609b79c4091c38822a",
+                "plone.app.contenttypes: Add Image",
+                ["Member"],
+            ],
+        ],
+    )
+    def test_permission_is_set(self, uid, permission_name, roles):
+        from plone.exportimport.utils.content import object_from_uid
+
+        content = object_from_uid(uid)
+        for role in roles:
+            all_permissions = [p["name"] for p in content.permissionsOfRole(role)]
+            assert permission_name in all_permissions
