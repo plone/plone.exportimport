82a9524d13ec711ac00eb54d30f8f97e5b9c7706
Érico Andrei <ericof@gmail.com>
2024-10-11 12:34:07 -0300

    Back to development: 1.0.0a9

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = f"""
 
 setup(
     name="plone.exportimport",
-    version="1.0.0a8",
+    version="1.0.0a9.dev0",
     description="Plone content export / import support",
     long_description=long_description,
     long_description_content_type="text/markdown",
