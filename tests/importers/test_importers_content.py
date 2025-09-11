358b0e74c849825f4a069338d0836beebd5903b3
Érico Andrei <ericof@gmail.com>
2025-09-11 16:44:13 +0200

    Revert "Fix import of ordering inside folderish components. (Fixes #60)"
    This reverts commit a5587a810a7b8ed056dc7f06a2579df2efaaf272.

--- tests/importers/test_importers_content.py
+++ tests/importers/test_importers_content.py
@@ -33,7 +33,7 @@ class TestImporterContent:
 
 class TestImporterLocalPermissions:
     @pytest.fixture(autouse=True)
-    def _init(self, portal, base_import_path):
+    def _init(self, portal, base_import_path, load_json):
         self.portal = portal
         importer = content.ContentImporter(portal)
         importer.import_data(base_path=base_import_path)
@@ -62,9 +62,43 @@ class TestImporterLocalPermissions:
             assert permission_name in all_permissions
 
 
+class TestImporterConstrains:
+    @pytest.fixture(autouse=True)
+    def _init(self, portal, base_import_path, load_json):
+        self.portal = portal
+        importer = content.ContentImporter(portal)
+        importer.import_data(base_path=base_import_path)
+
+    @pytest.mark.parametrize(
+        "uid,method,types",
+        [
+            [
+                "35661c9bb5be42c68f665aa1ed291418",
+                "getImmediatelyAddableTypes",
+                ["Image"],
+            ],
+            [
+                "35661c9bb5be42c68f665aa1ed291418",
+                "getLocallyAllowedTypes",
+                ["Document", "Image"],
+            ],
+        ],
+    )
+    def test_constrain_is_set(self, uid, method, types):
+        from plone.base.interfaces.constrains import ISelectableConstrainTypes
+        from plone.exportimport.utils.content import object_from_uid
+
+        content = object_from_uid(uid)
+        with api.env.adopt_roles(["Manager", "Site Administrator"]):
+            behavior = ISelectableConstrainTypes(content, None)
+            constrains = getattr(behavior, method)()
+        for type_ in types:
+            assert type_ in constrains
+
+
 class TestImporterParent:
     @pytest.fixture(autouse=True)
-    def _init(self, portal, base_import_path):
+    def _init(self, portal, base_import_path, load_json):
         self.portal = portal
         importer = content.ContentImporter(portal)
         importer.import_data(base_path=base_import_path)
@@ -104,65 +138,3 @@ class TestImporterParent:
         parent = get_parent_from_item(data)
         found_path = parent.absolute_url_path() if parent is not None else None
         assert found_path == path
-
-
-class TestImporterConstrains:
-    @pytest.fixture(autouse=True)
-    def _init(self, portal, base_import_path):
-        self.portal = portal
-        importer = content.ContentImporter(portal)
-        importer.import_data(base_path=base_import_path)
-
-    @pytest.mark.parametrize(
-        "uid,method,types",
-        [
-            [
-                "35661c9bb5be42c68f665aa1ed291418",
-                "getImmediatelyAddableTypes",
-                ["Image"],
-            ],
-            [
-                "35661c9bb5be42c68f665aa1ed291418",
-                "getLocallyAllowedTypes",
-                ["Document", "Image"],
-            ],
-        ],
-    )
-    def test_constrain_is_set(self, uid, method, types):
-        from plone.base.interfaces.constrains import ISelectableConstrainTypes
-        from plone.exportimport.utils.content import object_from_uid
-
-        content = object_from_uid(uid)
-        with api.env.adopt_roles(["Manager", "Site Administrator"]):
-            behavior = ISelectableConstrainTypes(content, None)
-            constrains = getattr(behavior, method)()
-        for type_ in types:
-            assert type_ in constrains
-
-
-class TestImporterOrdering:
-    """Test the ordering of imported items is correctly applied"""
-
-    @pytest.fixture(autouse=True)
-    def _init(self, portal, base_import_path):
-        self.portal = portal
-        importer = content.ContentImporter(portal)
-        importer.import_data(base_path=base_import_path)
-        # Search for the items at Portal root ordering by position in parent
-        brains = api.content.find(
-            context=portal, depth=1, sort_on="getObjPositionInParent"
-        )
-        self.items: list[str] = [b.UID for b in brains]
-
-    @pytest.mark.parametrize(
-        "uid,idx",
-        [
-            ["3e0dd7c4b2714eafa1d6fc6a1493f953", 0],
-            ["70844f7bec1843b8ab2796c972c9ebfe", 1],
-            ["e7359727ace64e609b79c4091c38822a", 2],
-        ],
-    )
-    def test_ordering_preserved(self, uid: str, idx: int):
-        """Test that each item is at the expected index within the Portal root."""
-        items: list[str] = self.items
-        assert items.index(uid) == idx
