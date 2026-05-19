3fcd37b295b21c10dc47af32cce16695965af748
Maurits van Rees <maurits@vanrees.org>
2026-05-19 10:37:12 +0200

    Back to development: 2.0.2
    [ci skip]

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = f"""
 
 setup(
     name="plone.exportimport",
-    version="2.0.1",
+    version="2.0.2.dev0",
     description="Plone content export / import support",
     long_description=long_description,
     long_description_content_type="text/markdown",
